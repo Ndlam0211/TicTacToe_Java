d366d52a4f671009cf11026ec5b752a0179aaff9
Nguyen Dinh Lam <nguyendinhlam0211@gmail.com>
2024-03-08 01:28:46 +0700

    modified project

--- src/main/Board.java
+++ src/main/Board.java
@@ -16,16 +16,55 @@ public class Board extends JPanel {
 
 	private Image imgX;
 	private Image imgO;
+	private Cell matrix[][] = new Cell[R][C];
+
+	// Người chơi đầu tiên đi O
+	private String currentPlayer;
+
+	public Board(String player) {
+		this();
+		this.currentPlayer = player;
+	}
 
 	public Board() {
+		this.initMatrix();
+
 		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
 				super.mousePressed(e);
-				System.out.println(e.getX());
-				System.out.println(e.getY());
+				int x = e.getX();
+				int y = e.getY();
+
+				// tính toán x,y(tọa độ) rơi vào ô nào trong board để render ra X,O tùy ý
+				for (int i = 0; i < R; i++)
+					for (int j = 0; j < C; j++) {
+						Cell cell = matrix[i][j];
+
+						int cXStart = cell.getX();
+						int cYStart = cell.getY();
+
+						int cXEnd = cXStart + cell.getW();
+						int cYEnd = cYStart + cell.getH();
+
+						if (x >= cXStart && x <= cXEnd) {
+							if (y >= cYStart && y <= cYEnd) {
+								if (cell.getValue().equals(Cell.EMPTY_VALUE)) {
+									// Cho người chơi đầu tiên
+									cell.setValue(currentPlayer);
+									// Đổi lượt chơi
+									currentPlayer = (currentPlayer.equals(Cell.O_VALUE)) ? Cell.X_VALUE : Cell.O_VALUE;
+									// gọi lại hàm paint để vẽ lại board
+									repaint();
+								}
+							}
+						}
+
+					}
+
 			}
 		});
+
 		try {
 			imgX = ImageIO.read(getClass().getResource("X.png"));
 			imgO = ImageIO.read(getClass().getResource("O.png"));
@@ -45,18 +84,38 @@ public class Board extends JPanel {
 		int k = 0;
 		for (int i = 0; i < R; i++)
 			for (int j = 0; j < C; j++) {
-				int x = i * w;
-				int y = j * h;
+				int x = j * w;
+				int y = i * h;
+
+				// cập nhật ma trận
+				Cell cell = matrix[i][j];
+				cell.setX(x);
+				cell.setY(y);
+				cell.setH(h);
+				cell.setW(w);
 
 				Color color = (k % 2 == 0) ? Color.BLUE : Color.RED;
 
 				graphics2d.setColor(color);
 				graphics2d.fillRect(x, y, w, h);
 
-				Image img = (k % 2 == 0) ? imgX : imgO;
-				graphics2d.drawImage(img, x, y, w, h, this);
+				if (cell.getValue().equals(Cell.X_VALUE)) {
+					Image img = imgX;
+					graphics2d.drawImage(img, x, y, w, h, this);
+				} else if (cell.getValue().equals(Cell.O_VALUE)) {
+					Image img = imgO;
+					graphics2d.drawImage(img, x, y, w, h, this);
+				}
 
 				k++;
+			}
+	}
+
+	private void initMatrix() {
+		for (int i = 0; i < R; i++)
+			for (int j = 0; j < C; j++) {
+				Cell cell = new Cell();
+				matrix[i][j] = cell;
 			}
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,27 +1,62 @@
 package main;
 
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Toolkit;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Board board;
+		int choice = JOptionPane.showConfirmDialog(null, "Người chơi O đi trước đúng không?", "Ai là người đi trước",
+				JOptionPane.YES_NO_OPTION);
+		if (choice == 0) {
+			board = new Board(Cell.O_VALUE);
+		} else {
+			board = new Board(Cell.X_VALUE);
+		}
 		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
 
-		Board board = new Board();
+		JPanel primaryPanel = new JPanel();
+
+		BoxLayout boxLayout = new BoxLayout(primaryPanel, BoxLayout.Y_AXIS);
+		primaryPanel.setLayout(boxLayout);
+
+		FlowLayout flowLayout = new FlowLayout(FlowLayout.CENTER, 0, 0);
+
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.setLayout(flowLayout);
+
+		board.setPreferredSize(new Dimension(600, 600));
+
+		JButton btnStart = new JButton("start");
+		JLabel lbTime = new JLabel("00:00");
+		bottomPanel.add(lbTime);
+		bottomPanel.add(btnStart);
+
+		primaryPanel.add(board);
+		primaryPanel.add(bottomPanel);
 
 		JFrame jframe = new JFrame("Tic Tac Toe Game");
-		jframe.setSize(600, 600);
-		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jframe.setResizable(false);
-		jframe.add(board);
 
-		int w = (int) (dimension.getWidth() / 2) - (jframe.getWidth() / 2);
-		int h = (int) (dimension.getHeight() / 2) - (jframe.getHeight() / 2);
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.setResizable(true);
+		jframe.add(primaryPanel);
+
+		int w = (int) (dimension.getWidth() / 2) - 300;
+		int h = (int) (dimension.getHeight() / 2) - 300;
 
 		jframe.setLocation(w, h);
+		// help frame scale according to internal elements
+		jframe.pack();
+		// show screen
 		jframe.setVisible(true);
 	}
 
